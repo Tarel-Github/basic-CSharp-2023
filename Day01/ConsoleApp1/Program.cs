eb232960343923f4a2aa87372428adf2c77252f3
Tarel-Github <sms_stmax@naver.com>
2023-04-04 12:15:01 +0900

    C# 첫번째 커밋

---
+++ Day01/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// 네임스페이스 ConsoleApp1
+namespace ConsoleApp1
+{
+    // 슬레시 3개는 나중에 문서화할때 사용하는 특별한 주석이다.
+    /// <summary>
+    /// 프로그램 클래스
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// 메인 엔트리 포인트
+        /// </summary>
+        /// <param name="args">콘솔 매개변수</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!!");
+        }
+    }
+}
